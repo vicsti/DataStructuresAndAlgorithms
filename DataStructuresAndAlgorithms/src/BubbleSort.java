7186664219c884bb89544a531acbae68587c615e
Jasper <Jasper@172.31.99.30>
2018-09-01 16:02:44 -0400

    Bubble Sort- O(N2) time complexity

--- DataStructuresAndAlgorithms/src/BubbleSort.java
+++ DataStructuresAndAlgorithms/src/BubbleSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class BubbleSort {
 	
@@ -11,5 +12,28 @@ public class BubbleSort {
 	// i. sorted and unsorted Partitions.
 	//
 	//
+	
+	public static void main(String[] args) {
+		int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+		
+		for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0 ; lastUnsortedIndex--) {
+			for(int i = 0; i< lastUnsortedIndex; i++ ) {
+				if(intArray[i] > intArray[i+1]) {
+					swap(intArray, i, i+1);
+				}
+			}			
+		}
+		System.out.println(Arrays.toString(intArray));
+	}
+	
+	public static void swap(int[] array, int i, int j) {
+		if (i == j) {
+			return;
+		}
+		int temp = array[i];
+		array[i]= array[j];
+		array[j] = temp;
+		
+	}
 
 }
