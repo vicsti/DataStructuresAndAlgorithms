ab7e2b8009ed1248758eb6c6776b738fc7157fae
Jasper <Jasper@172.31.99.30>
2018-09-01 15:44:36 -0400

    Practice DataStructures and Algorithms in Java

---
+++ DataStructuresAndAlgorithms/src/BubbleSort.java
@@ -0,0 +1,15 @@
+
+public class BubbleSort {
+	
+	//Bubble sort os not the best of the sorts
+	// ex Array: 20, 35, -15, 7, 55, 1, -22
+	// In bubble sort we compare i, i+1 and then swap 
+	// the element to the right if the a[i]>a[i+1] and then
+	//increment the index i.
+	// We then know that the largest number is at the right most element
+	// so we divide the array into two logical partition
+	// i. sorted and unsorted Partitions.
+	//
+	//
+
+}
