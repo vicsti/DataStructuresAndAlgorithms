a77dfd58f10f45be49e452ef37cfceee81b7be5b
Jasper <Jasper@172.31.99.30>
2018-09-06 12:41:42 -0400

    Dlinked List - added a function addBefore

--- DataStructuresAndAlgorithms/src/doublylinkedlist/EmployeeDlinkedList.java
+++ DataStructuresAndAlgorithms/src/doublylinkedlist/EmployeeDlinkedList.java
@@ -3,27 +3,66 @@ package doublylinkedlist;
 public class EmployeeDlinkedList {
 	EmployeeNode head;	
 	EmployeeNode tail;
+	static int size;
 	
 	public EmployeeDlinkedList(Employee emp) {
 		EmployeeNode empNode = new EmployeeNode(emp);
 		head = tail = empNode;
 		empNode.next=null;
-		empNode.prev=null;				
+		empNode.prev=null;	
+		size++;
 	}
 	
 	public void addFirst(Employee emp) {
 		EmployeeNode empNode = new EmployeeNode(emp);
 		head.prev=empNode;
 		empNode.next=head;
-		head=empNode;		
+		head=empNode;	
+		size++;
 	}
 	
 	public void addLast(Employee emp) {
 		EmployeeNode empNode = new EmployeeNode(emp);
 		tail.next=empNode;
 		empNode.prev=tail;
-		tail=empNode;		
+		tail=empNode;	
+		size++;
 	}	
+	
+	public boolean addBefore(Employee empToAdd, Employee empRef) {
+		EmployeeNode start=head;
+		while(start.emp != empRef && start!=null) {
+			start=start.next;
+		}
+		
+		if(start.emp == empRef) {
+			EmployeeNode empNode = new EmployeeNode(empToAdd);
+			empNode.next=start;
+			empNode.prev=start.prev;
+			start.prev.next=empNode;
+			start.prev=empNode;	
+			return true;
+		} else {
+			return false;
+		}		
+	}
+	
+	public void removeFirst() {
+		head=head.next;
+		head.prev=null;
+		size--;
+		
+	}
+	
+	public void removeLast() {
+		tail=tail.prev;
+		tail.next=null;
+		size--;		
+	}
+	
+	public int getSize() {
+		return size;
+	}
 	
 	public void printList() {
 		EmployeeNode current = head;
@@ -33,7 +72,8 @@ public class EmployeeDlinkedList {
 			System.out.print(current.emp.toString());
 			System.out.print("--");	
 			current=current.next;
-		}		
+		}
+		System.out.println();
 	}
 
 }
--- DataStructuresAndAlgorithms/src/doublylinkedlist/MainDlinkList.java
+++ DataStructuresAndAlgorithms/src/doublylinkedlist/MainDlinkList.java
@@ -9,6 +9,8 @@ public class MainDlinkList {
 		Employee emp3 = new Employee("Jasper", "Daniel", 3);
 		Employee emp4 = new Employee("Allwyn", "Peter", 4);
 		Employee emp5 = new Employee("Sharon", "Allwyn", 5);
+		Employee emp6 = new Employee("Selvin", "Raj", 6);
+		Employee empToAdd = new Employee("Victoria", "Selvin", 7);
 		
 		EmployeeDlinkedList empDlist = new EmployeeDlinkedList(emp1);
 		
@@ -16,8 +18,23 @@ public class MainDlinkList {
 		empDlist.addLast(emp4);
 		empDlist.addLast(emp3);
 		empDlist.addFirst(emp5);
+		empDlist.addFirst(emp6);
+		//empDlist.addFirst(emp7);
 		
 		empDlist.printList();
+		System.out.println(empDlist.getSize());
+		
+		//empDlist.removeFirst();
+		//empDlist.printList();
+		//System.out.println(empDlist.getSize());
+		
+		//empDlist.removeLast();
+		//empDlist.printList();
+		//System.out.println(empDlist.getSize());
+		
+		empDlist.addBefore(empToAdd, emp4);
+		empDlist.printList();		
+		
 	}
 
 }
