83bcdc119ba2471a4420b7feb0f3fcc318d3d3e7
Jasper <Jasper@172.31.99.30>
2018-09-06 17:32:13 -0400

    Palindrome check implementation using stacks

--- DataStructuresAndAlgorithms/src/stachproblem1/MainSolution.java
+++ DataStructuresAndAlgorithms/src/stachproblem1/MainSolution.java
@@ -7,23 +7,30 @@ public class MainSolution {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String input = "MALAYALA";
+		String input = "Dont't nods";
+		String lowerCase = input.toLowerCase();
+		StringBuilder newStr = new StringBuilder(input.length());
 		LinkedList<Character> stack = new LinkedList<Character>();
 		
-		for(int i=0; i<input.length();i++) {
-			stack.push(input.charAt(i));
+			
+		for(int i=0; i<lowerCase.length();i++) {
+			if(lowerCase.charAt(i) >= 'a' && lowerCase.charAt(i) <= 'z') {
+				newStr.append(lowerCase.charAt(i));
+			stack.push(lowerCase.charAt(i));
+			}
 			}
 		
-		checkPal(input, stack);
+		checkPal(newStr.toString(), stack);
 
 		
 
 	}
 	
 	public static boolean checkPal(String input, LinkedList<Character> stack) {
-		int i=0;
-		for (Character in : stack) {
-			if(in != input.charAt(i++)) {
+		
+		for (int i=0; i<input.length();i++) {
+			//System.out.println(in+" "+input.charAt(i));
+			if(stack.pop() != input.charAt(i)) {
 				System.out.println("Not a pal");
 				return false;
 			}
