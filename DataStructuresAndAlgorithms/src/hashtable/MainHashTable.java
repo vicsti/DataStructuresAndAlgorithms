c9cbf0d74fb580e46c0c8a198a20b6868b953271
Jasper <Jasper@172.31.99.30>
2018-09-07 09:06:37 -0400

    HashTables

---
+++ DataStructuresAndAlgorithms/src/hashtable/MainHashTable.java
@@ -0,0 +1,15 @@
+package hashtable;
+
+public class MainHashTable {
+	//Abstract data type
+	//Provides access to data using keys
+	//Keys dont have to be inyteger
+	//Consisit of key value pairs, the data types dont have to match
+	//Optimized for retrival (When you know the key)
+	//Assosiative array is one type of hash table
+	//
+	//
+	//
+	//
+
+}
