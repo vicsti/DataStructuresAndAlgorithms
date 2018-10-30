078077d513dfb8c21530be01d749c136070ba33b
Jasper <Jasper@172.31.99.31>
2018-10-30 18:42:37 -0400

    Pal number

--- DataStructuresAndAlgorithms/src/palindromnumber/palindromNumber.java
+++ DataStructuresAndAlgorithms/src/palindromnumber/palindromNumber.java
@@ -4,13 +4,18 @@ public class palindromNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		if(11 == palindromNumber.checkPalindrom(11, 0)) {
+			System.out.print("IS A PAL");
+		} else {
+			System.out.print("NOT A PAL");
+		}
 
 	}
 	
-	public static int checkPalindrom(int num){
-		int rev=0;
-		if(num==0) return 1;
-		return checkPalindrom(num/10) + num%10;
+	public static int checkPalindrom(int num, int rev){
+		if(num/10==0) return (rev*10 + num%10);
+		rev = rev*10 + num%10;
+		return checkPalindrom(num/10, rev);
 		//rev = rev*10 + num%10;
 	}
 
