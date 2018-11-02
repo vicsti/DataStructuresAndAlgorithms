ab5ab169d567b883f3330f7e9e44ab2858e63a70
Jasper <Jasper@172.31.99.31>
2018-11-02 10:25:40 -0400

    N queen Problem

---
+++ DataStructuresAndAlgorithms/src/nqueenproblem/Board.java
@@ -0,0 +1,32 @@
+package nqueenproblem;
+
+public class Board {
+
+	public int getMaxCol() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getMaxRow() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean isSafe(int row, int col) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void placeQueen(int row, int col) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeQueen(int row, int col) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
---
+++ DataStructuresAndAlgorithms/src/nqueenproblem/nqueenProblem.java
@@ -0,0 +1,27 @@
+package nqueenproblem;
+
+public class nqueenProblem {
+	
+	public static void main(String[] args) {
+		
+	}
+
+	public static boolean solve(Board board, int col) {
+		
+		if(col >= board.getMaxCol()) {
+			return true;
+		}
+		
+		for(int row=0;row<=board.getMaxRow();row++) {
+			if(board.isSafe(row, col)) {
+				board.placeQueen(row, col);
+				if(solve(board, col+1)) return true;
+				board.removeQueen(row, col);
+				
+			}
+		}
+		
+		return false;
+		
+	}
+}
